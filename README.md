a9eec823d27c73cc4c6d80f9332506864482f72d
stevemacenski <stevenmacenski@gmail.com>
2020-02-06 21:38:02 -0800

    Revert "Trimmed the branch to only the nav2_map_server sub directory"
    This reverts commit e054b68b3c10b7d741db357daae427763d100de3.

--- README.md
+++ README.md
@@ -1,97 +1,46 @@
-# Map Server
+# Navigation2
 
-The `Map Server` provides maps to the rest of the Navigation2 system using both topic and
-service interfaces.
+ROS2 Navigation System
 
-## Changes from ROS1 Navigation Map Server
+[![Build Status](https://circleci.com/gh/ros-planning/navigation2/tree/master.svg?style=svg)](https://circleci.com/gh/ros-planning/navigation2/tree/master) CircleCI
 
-While the nav2 map server provides the same general function as the nav1 map server, the new
-code has some changes to accomodate ROS2 as well as some architectural improvements.
+[![Build Status](https://img.shields.io/docker/cloud/build/rosplanning/navigation2.svg?label=build)](https://hub.docker.com/r/rosplanning/navigation2) DockerHub
 
-In addition, there is now a new "load_map" service which can be used to dynamically load a map.
+[![Build Status](http://build.ros2.org/job/Cdev__navigation2__ubuntu_bionic_amd64/badge/icon)](http://build.ros2.org/job/Cdev__navigation2__ubuntu_bionic_amd64/) ROS Build Farm 
 
-### Architecture
+[![codecov](https://codecov.io/gh/ros-planning/navigation2/branch/master/graph/badge.svg)](https://codecov.io/gh/ros-planning/navigation2)
 
-In contrast to the ROS1 navigation map server, the nav2 map server will support a variety
-of map types, and thus some aspects of the original code have been refactored to support
-this new extensible framework. In particular, there is now a `MapLoader` abstract base class
-and type-specific map loaders which derive from this class. There is currently one such
-derived class, the `OccGridLoader`, which converts an input image to an OccupancyGrid and
-makes this available via topic and service interfaces. The `MapServer` class is a ROS2 node
-that uses the appropriate loader, based on an input parameter.
+# Overview
+The ROS 2 Navigation System is the control system that enables a robot to autonomously reach a goal state, such as a specific position and orientation relative to a specific map. Given a current pose, a map, and a goal, such as a destination pose, the navigation system generates a plan to reach the goal, and outputs commands to autonomously drive the robot, respecting any safety constraints and avoiding obstacles encountered along the way.
 
-### Command-line arguments, ROS2 Node Parameters, and YAML files
+![nav2_overview](doc/architecture/navigation_overview.png)
 
-The Map Server is a composable ROS2 node. By default, there is a map_server executable that
-instances one of these nodes, but it is possible to compose multiple map server nodes into
-a single process, if desired.
+# Documentation
+For detailed instructions on how to install and run the examples, please visit our [documentation site](https://ros-planning.github.io/navigation2/).
 
-The command line for the map server executable is slightly different that it was with ROS1.
-With ROS1, one invoked the map server and passing the map YAML filename, like this:
+# Contributing
+[Contributions are welcome!](doc/README.md#contributing). For more information, please review our [contribution guidelines](https://ros-planning.github.io/navigation2/contribute/contribute_guidelines.html).
+
+# Building the source
+For instructions on how to download and build this repo, see the [BUILD.md](doc/BUILD.md) file.
+
+# Creating a docker image
+To build an image from the Dockerfile in the navigation2 folder:
+First, clone the repo to your local system (or see Building the source above)
+```
+sudo docker build -t nav2/latest .
+```
+If proxies are needed:
+```
+sudo docker build -t nav2/latest --build-arg http_proxy=http://proxy.my.com:### --build-arg https_proxy=http://proxy.my.com:### .
+```
+Note: You may also need to configure your docker for DNS to work. See article here for details:
+https://development.robinwinslow.uk/2016/06/23/fix-docker-networking-dns/
+
+## Using CI build docker container
+
+We allow for you to pull the latest docker image from the master branch at any time. As new releases and tags are made, docker containers on docker hub will be versioned as well to chose from.
 
 ```
-$ map_server map.yaml
+sudo docker pull rosplanning/navigation2:latest
 ```
-
-Where the YAML file specified contained the various map metadata, such as:
-
-```
-image: testmap.png
-resolution: 0.1
-origin: [2.0, 3.0, 1.0]
-negate: 0
-occupied_thresh: 0.65
-free_thresh: 0.196
-```
-
-The Navigation2 software retains the map YAML file format from Nav1, but uses the ROS2 parameter
-mechanism to get the name of the YAML file to use. This effectively introduces a
-level of indirection to get the map yaml filename. For example, for a node named 'map_server',
-the parameter file would look like this:
-
-```
-# map_server_params.yaml
-map_server:
-    ros__parameters:
-        yaml_filename: "map.yaml"
-```
-
-One can invoke the map service executable directly, passing the params file on the command line,
-like this:
-
-```
-$ map_server __params:=map_server_params.yaml
-```
-
-There is also possibility of having multiple map server nodes in a single process, where the parameters file would separate the parameters by node name, like this:
-
-```
-# combined_params.yaml
-map_server1:
-    ros__parameters:
-        yaml_filename: "some_map.yaml"
-
-map_server2:
-    ros__parameters:
-        yaml_filename: "another_map.yaml"
-```
-
-Then, one would invoke this process with the params file that contains the parameters for both nodes:
-
-```
-$ process_with_multiple_map_servers __params:=combined_params.yaml
-```
-
-## Currently Supported Map Types
-- Occupancy grid (nav_msgs/msg/OccupancyGrid), via the OccGridLoader
-
-## Services
-As in ROS navigation, the map_server node provides a "map" service to get the map. See the nav_msgs/srv/GetMap.srv file for details.
-
-NEW in ROS2 Eloquent, map_server also now provides a "load_map" service. See nav2_msgs/srv/LoadMap.srv for details.
-
-Example:
-```
-$ ros2 service call /load_map nav2_msgs/srv/LoadMap "{type: 0, map_id: /ros/maps/map.yaml}
-```
-
---
+++ nav2_planner/src/planner_server.cpp
@@ -0,0 +1,287 @@
+// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2019 Samsung Research America
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "builtin_interfaces/msg/duration.hpp"
+#include "nav2_util/costmap.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "nav2_costmap_2d/cost_values.hpp"
+
+#include "nav2_planner/planner_server.hpp"
+
+using namespace std::chrono_literals;
+
+namespace nav2_planner
+{
+
+PlannerServer::PlannerServer()
+: nav2_util::LifecycleNode("nav2_planner", "", true),
+  gp_loader_("nav2_core", "nav2_core::GlobalPlanner"), costmap_(nullptr)
+{
+  RCLCPP_INFO(get_logger(), "Creating");
+
+  // Declare this node's parameters
+  std::vector<std::string> default_id, default_type;
+  default_id.push_back("GridBased");
+  default_type.push_back("nav2_navfn_planner/NavfnPlanner");
+  declare_parameter("planner_plugin_ids", default_id);
+  declare_parameter("planner_plugin_types", default_type);
+
+  // Setup the global costmap
+  costmap_ros_ = std::make_shared<nav2_costmap_2d::Costmap2DROS>(
+    "global_costmap", std::string{get_namespace()}, "global_costmap");
+
+  // Launch a thread to run the costmap node
+  costmap_thread_ = std::make_unique<nav2_util::NodeThread>(costmap_ros_);
+}
+
+PlannerServer::~PlannerServer()
+{
+  RCLCPP_INFO(get_logger(), "Destroying");
+  PlannerMap::iterator it;
+  for (it = planners_.begin(); it != planners_.end(); ++it) {
+    it->second.reset();
+  }
+}
+
+nav2_util::CallbackReturn
+PlannerServer::on_configure(const rclcpp_lifecycle::State & state)
+{
+  RCLCPP_INFO(get_logger(), "Configuring");
+
+  costmap_ros_->on_configure(state);
+  costmap_ = costmap_ros_->getCostmap();
+
+  RCLCPP_DEBUG(get_logger(), "Costmap size: %d,%d",
+    costmap_->getSizeInCellsX(), costmap_->getSizeInCellsY());
+
+  tf_ = costmap_ros_->getTfBuffer();
+
+  get_parameter("planner_plugin_ids", plugin_ids_);
+  get_parameter("planner_plugin_types", plugin_types_);
+  auto node = shared_from_this();
+
+  if (plugin_ids_.size() != plugin_types_.size()) {
+    RCLCPP_FATAL(get_logger(),
+      "Planner plugin names and types sizes do not match!");
+    exit(-1);
+  }
+
+  for (uint i = 0; i != plugin_types_.size(); i++) {
+    try {
+      nav2_core::GlobalPlanner::Ptr planner =
+        gp_loader_.createUniqueInstance(plugin_types_[i]);
+      RCLCPP_INFO(get_logger(), "Created global planner plugin %s of type %s",
+        plugin_ids_[i].c_str(), plugin_types_[i].c_str());
+      planner->configure(node, plugin_ids_[i], tf_, costmap_ros_);
+      planners_.insert({plugin_ids_[i], planner});
+    } catch (const pluginlib::PluginlibException & ex) {
+      RCLCPP_FATAL(get_logger(), "Failed to create global planner. Exception: %s",
+        ex.what());
+      exit(-1);
+    }
+  }
+
+  for (uint i = 0; i != plugin_types_.size(); i++) {
+    planner_ids_concat_ += plugin_ids_[i] + std::string(" ");
+  }
+
+  // Initialize pubs & subs
+  plan_publisher_ = create_publisher<nav_msgs::msg::Path>("plan", 1);
+
+  // Create the action server that we implement with our navigateToPose method
+  action_server_ = std::make_unique<ActionServer>(rclcpp_node_,
+      "compute_path_to_pose",
+      std::bind(&PlannerServer::computePlan, this));
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+PlannerServer::on_activate(const rclcpp_lifecycle::State & state)
+{
+  RCLCPP_INFO(get_logger(), "Activating");
+
+  plan_publisher_->on_activate();
+  action_server_->activate();
+  costmap_ros_->on_activate(state);
+
+  PlannerMap::iterator it;
+  for (it = planners_.begin(); it != planners_.end(); ++it) {
+    it->second->activate();
+  }
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+PlannerServer::on_deactivate(const rclcpp_lifecycle::State & state)
+{
+  RCLCPP_INFO(get_logger(), "Deactivating");
+
+  action_server_->deactivate();
+  plan_publisher_->on_deactivate();
+  costmap_ros_->on_deactivate(state);
+
+  PlannerMap::iterator it;
+  for (it = planners_.begin(); it != planners_.end(); ++it) {
+    it->second->deactivate();
+  }
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+PlannerServer::on_cleanup(const rclcpp_lifecycle::State & state)
+{
+  RCLCPP_INFO(get_logger(), "Cleaning up");
+
+  action_server_.reset();
+  plan_publisher_.reset();
+  tf_.reset();
+  costmap_ros_->on_cleanup(state);
+
+  PlannerMap::iterator it;
+  for (it = planners_.begin(); it != planners_.end(); ++it) {
+    it->second->cleanup();
+  }
+  planners_.clear();
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+PlannerServer::on_error(const rclcpp_lifecycle::State &)
+{
+  RCLCPP_FATAL(get_logger(), "Lifecycle node entered error state");
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+PlannerServer::on_shutdown(const rclcpp_lifecycle::State &)
+{
+  RCLCPP_INFO(get_logger(), "Shutting down");
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+void
+PlannerServer::computePlan()
+{
+  // Initialize the ComputePathToPose goal and result
+  auto goal = action_server_->get_current_goal();
+  auto result = std::make_shared<nav2_msgs::action::ComputePathToPose::Result>();
+
+  try {
+    if (action_server_ == nullptr) {
+      RCLCPP_DEBUG(get_logger(), "Action server unavailable. Stopping.");
+      return;
+    }
+
+    if (!action_server_->is_server_active()) {
+      RCLCPP_DEBUG(get_logger(), "Action server is inactive. Stopping.");
+      return;
+    }
+
+    if (action_server_->is_cancel_requested()) {
+      RCLCPP_INFO(get_logger(), "Goal was canceled. Canceling planning action.");
+      action_server_->terminate_all();
+      return;
+    }
+
+    geometry_msgs::msg::PoseStamped start;
+    if (!nav2_util::getCurrentPose(start, *tf_)) {
+      return;
+    }
+
+    if (action_server_->is_preempt_requested()) {
+      RCLCPP_INFO(get_logger(), "Preempting the goal pose.");
+      goal = action_server_->accept_pending_goal();
+    }
+
+    RCLCPP_DEBUG(get_logger(), "Attempting to a find path from (%.2f, %.2f) to "
+      "(%.2f, %.2f).", start.pose.position.x, start.pose.position.y,
+      goal->pose.pose.position.x, goal->pose.pose.position.y);
+
+    if (planners_.find(goal->planner_id) != planners_.end()) {
+      result->path = planners_[goal->planner_id]->createPlan(start, goal->pose);
+    } else {
+      if (planners_.size() == 1 && goal->planner_id.empty()) {
+        if (!single_planner_warning_given_) {
+          single_planner_warning_given_ = true;
+          RCLCPP_WARN(get_logger(), "No planners specified in action call. "
+            "Server will use only plugin %s in server."
+            " This warning will appear once.", planner_ids_concat_.c_str());
+        }
+        result->path = planners_[planners_.begin()->first]->createPlan(start, goal->pose);
+      } else {
+        RCLCPP_ERROR(get_logger(), "planner %s is not a valid planner. "
+          "Planner names are: %s", goal->planner_id.c_str(),
+          planner_ids_concat_.c_str());
+      }
+    }
+
+    if (result->path.poses.size() == 0) {
+      RCLCPP_WARN(get_logger(), "Planning algorithm %s failed to generate a valid"
+        " path to (%.2f, %.2f)", goal->planner_id.c_str(),
+        goal->pose.pose.position.x, goal->pose.pose.position.y);
+      action_server_->terminate_current();
+      return;
+    }
+
+    RCLCPP_DEBUG(get_logger(),
+      "Found valid path of size %u to (%.2f, %.2f)",
+      result->path.poses.size(), goal->pose.pose.position.x,
+      goal->pose.pose.position.y);
+
+    // Publish the plan for visualization purposes
+    RCLCPP_DEBUG(get_logger(), "Publishing the valid path");
+    publishPlan(result->path);
+
+    action_server_->succeeded_current(result);
+    return;
+  } catch (std::exception & ex) {
+    RCLCPP_WARN(get_logger(), "%s plugin failed to plan calculation to (%.2f, %.2f): \"%s\"",
+      goal->planner_id.c_str(), goal->pose.pose.position.x,
+      goal->pose.pose.position.y, ex.what());
+    // TODO(orduno): provide information about fail error to parent task,
+    //               for example: couldn't get costmap update
+    action_server_->terminate_current();
+    return;
+  } catch (...) {
+    RCLCPP_WARN(get_logger(), "Plan calculation failed, "
+      "An unexpected error has occurred. The planner server"
+      " may not be able to continue operating correctly.");
+    // TODO(orduno): provide information about fail error to parent task,
+    //               for example: couldn't get costmap update
+    action_server_->terminate_current();
+    return;
+  }
+}
+
+void
+PlannerServer::publishPlan(const nav_msgs::msg::Path & path)
+{
+  plan_publisher_->publish(path);
+}
+
+}  // namespace nav2_planner
