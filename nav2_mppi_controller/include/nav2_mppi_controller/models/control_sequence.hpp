e6419a9c3db7504b0ab5ce51004aedbbd7f675e6
Steve Macenski <stevenmacenski@gmail.com>
2024-06-25 04:31:59 +0000

    Revert "Trimmed the branch to only the nav2_controller sub directory"
    This reverts commit a481453653784f32a2ff03b3b1a0a5a1d0f46693.

---
+++ nav2_mppi_controller/include/nav2_mppi_controller/models/control_sequence.hpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2022 Samsung Research America, @artofnothingness Alexey Budyakov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_MPPI_CONTROLLER__MODELS__CONTROL_SEQUENCE_HPP_
+#define NAV2_MPPI_CONTROLLER__MODELS__CONTROL_SEQUENCE_HPP_
+
+// xtensor creates warnings that needs to be ignored as we are building with -Werror
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
+#pragma GCC diagnostic ignored "-Wstringop-overflow"
+#include <xtensor/xtensor.hpp>
+#pragma GCC diagnostic pop
+
+namespace mppi::models
+{
+
+/**
+ * @struct mppi::models::Control
+ * @brief A set of controls
+ */
+struct Control
+{
+  float vx, vy, wz;
+};
+
+/**
+ * @struct mppi::models::ControlSequence
+ * @brief A control sequence over time (e.g. trajectory)
+ */
+struct ControlSequence
+{
+  xt::xtensor<float, 1> vx;
+  xt::xtensor<float, 1> vy;
+  xt::xtensor<float, 1> wz;
+
+  void reset(unsigned int time_steps)
+  {
+    vx = xt::zeros<float>({time_steps});
+    vy = xt::zeros<float>({time_steps});
+    wz = xt::zeros<float>({time_steps});
+  }
+};
+
+}  // namespace mppi::models
+
+#endif  // NAV2_MPPI_CONTROLLER__MODELS__CONTROL_SEQUENCE_HPP_
