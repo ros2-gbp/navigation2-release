c6e0fc759b209364c87f50e67935e91a0173c697
SteveMacenski <stevenmacenski@gmail.com>
2026-08-12 00:49:12 +0000

    Revert "Trimmed the branch to only the nav2_docking/opennav_docking_core sub directory"
    This reverts commit 89aeadb769247b043f6e57a65965219b774cf179.

---
+++ nav2_behavior_tree/test/plugins/condition/test_is_battery_low.cpp
@@ -0,0 +1,173 @@
+// Copyright (c) 2020 Sarthak Mittal
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <set>
+#include <string>
+#include <chrono>
+
+#include "sensor_msgs/msg/battery_state.hpp"
+
+#include "utils/test_behavior_tree_fixture.hpp"
+#include "nav2_behavior_tree/plugins/condition/is_battery_low_condition.hpp"
+
+class IsBatteryLowConditionTestFixture : public ::testing::Test
+{
+public:
+  static void SetUpTestCase()
+  {
+    node_ = std::make_shared<nav2::LifecycleNode>("test_is_battery_low");
+    executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+    executor_->add_node(node_->get_node_base_interface());
+    factory_ = std::make_shared<BT::BehaviorTreeFactory>();
+
+    config_ = new BT::NodeConfiguration();
+
+    // Create the blackboard that will be shared by all of the nodes in the tree
+    config_->blackboard = BT::Blackboard::create();
+    // Put items on the blackboard
+    config_->blackboard->set(
+      "node",
+      node_);
+    config_->blackboard->set<std::chrono::milliseconds>(
+      "bt_loop_duration",
+      std::chrono::milliseconds(10));
+
+    factory_->registerNodeType<nav2_behavior_tree::IsBatteryLowCondition>("IsBatteryLow");
+
+    battery_pub_ = node_->create_publisher<sensor_msgs::msg::BatteryState>(
+      "/battery_status",
+      rclcpp::SystemDefaultsQoS());
+    battery_pub_->on_activate();
+  }
+
+  static void TearDownTestCase()
+  {
+    delete config_;
+    config_ = nullptr;
+    battery_pub_.reset();
+    node_.reset();
+    factory_.reset();
+    executor_.reset();
+  }
+
+protected:
+  static nav2::LifecycleNode::SharedPtr node_;
+  static rclcpp::executors::SingleThreadedExecutor::SharedPtr executor_;
+  static BT::NodeConfiguration * config_;
+  static std::shared_ptr<BT::BehaviorTreeFactory> factory_;
+  static nav2::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr
+    battery_pub_;
+};
+
+nav2::LifecycleNode::SharedPtr IsBatteryLowConditionTestFixture::node_ = nullptr;
+rclcpp::executors::SingleThreadedExecutor::SharedPtr IsBatteryLowConditionTestFixture::
+executor_ = nullptr;
+BT::NodeConfiguration * IsBatteryLowConditionTestFixture::config_ = nullptr;
+std::shared_ptr<BT::BehaviorTreeFactory> IsBatteryLowConditionTestFixture::factory_ = nullptr;
+nav2::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr
+IsBatteryLowConditionTestFixture::battery_pub_ = nullptr;
+
+TEST_F(IsBatteryLowConditionTestFixture, test_behavior_percentage)
+{
+  std::string xml_txt =
+    R"(
+      <root BTCPP_format="4">
+        <BehaviorTree ID="MainTree">
+            <IsBatteryLow min_battery="0.5" battery_topic="/battery_status"/>
+        </BehaviorTree>
+      </root>)";
+
+  auto tree = factory_->createTreeFromText(xml_txt, config_->blackboard);
+
+  sensor_msgs::msg::BatteryState battery_msg;
+  battery_msg.percentage = 1.0;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::FAILURE);
+
+  battery_msg.percentage = 0.49;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::SUCCESS);
+
+  battery_msg.percentage = 0.51;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::FAILURE);
+
+  battery_msg.percentage = 0.0;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::SUCCESS);
+}
+
+TEST_F(IsBatteryLowConditionTestFixture, test_behavior_voltage)
+{
+  std::string xml_txt =
+    R"(
+      <root BTCPP_format="4">
+        <BehaviorTree ID="MainTree">
+            <IsBatteryLow min_battery="5.0" battery_topic="/battery_status" is_voltage="true"/>
+        </BehaviorTree>
+      </root>)";
+
+  auto tree = factory_->createTreeFromText(xml_txt, config_->blackboard);
+
+  sensor_msgs::msg::BatteryState battery_msg;
+  battery_msg.voltage = 10.0;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::FAILURE);
+
+  battery_msg.voltage = 4.9;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::SUCCESS);
+
+  battery_msg.voltage = 5.1;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::FAILURE);
+
+  battery_msg.voltage = 0.0;
+  battery_pub_->publish(battery_msg);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  executor_->spin_some();
+  EXPECT_EQ(tree.tickOnce(), BT::NodeStatus::SUCCESS);
+}
+
+int main(int argc, char ** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  // initialize ROS
+  rclcpp::init(argc, argv);
+
+  bool all_successful = RUN_ALL_TESTS();
+
+  // shutdown ROS
+  rclcpp::shutdown();
+
+  return all_successful;
+}
---
+++ nav2_util/src/lifecycle_bringup_commandline.cpp
@@ -0,0 +1,96 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_util/lifecycle_service_client.hpp"
+
+using std::cerr;
+using namespace std::chrono_literals;  // NOLINT
+using namespace nav2_util;  // NOLINT
+
+void usage()
+{
+  cerr << "Invalid command line.\n\n";
+  cerr << "This command will take a set of unconfigured lifecycle nodes through the\n";
+  cerr << "CONFIGURED to the ACTIVATED state\n";
+  cerr << "The nodes are brought up in the order listed on the command line\n\n";
+  cerr << "Usage:\n";
+  cerr << " > lifecycle_startup <node name> ...\n";
+  std::exit(1);
+}
+
+#define RETRY(fn, retries) \
+  { \
+    int count = 0; \
+    while (true) { \
+      try { \
+        fn; \
+        break; \
+      } catch (const std::runtime_error & e) { \
+        ++count; \
+        if (count > (retries)) { \
+          throw e;} \
+      } \
+    } \
+  }
+
+inline void startupLifecycleNode(
+  rclcpp::Node::SharedPtr node,  //  nosemgrep
+  const std::string & node_name,
+  const std::chrono::seconds service_call_timeout,
+  const int retries)
+{
+  LifecycleServiceClient sc(node_name, node);
+
+  // Despite waiting for the service to be available and using reliable transport
+  // service calls still frequently hang. To get reliable startup it's necessary
+  // to timeout the service call and retry it when that happens.
+  RETRY(
+    sc.change_state(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE, service_call_timeout),
+    retries);
+  RETRY(
+    sc.change_state(lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE, service_call_timeout),
+    retries);
+}
+
+inline void startup_lifecycle_nodes(
+  rclcpp::Node::SharedPtr node,  //  nosemgrep
+  const std::vector<std::string> & node_names,
+  const std::chrono::seconds service_call_timeout,
+  const int retries = 3)
+{
+  for (const auto & node_name : node_names) {
+    startupLifecycleNode(node, node_name, service_call_timeout, retries);
+  }
+}
+
+
+int main(int argc, char * argv[])
+{
+  if (argc == 1) {
+    usage();
+  }
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("lifecycle_bringup_client");  //  nosemgrep
+  startup_lifecycle_nodes(
+    node,
+    std::vector<std::string>(argv + 1, argv + argc),
+    10s);
+  rclcpp::shutdown();
+}
