6c1b57c13f09040d38e752a08faac44b6eb9dc6e
SteveMacenski <stevenmacenski@gmail.com>
2026-08-12 00:49:17 +0000

    Revert "Trimmed the branch to only the nav2_voxel_grid sub directory"
    This reverts commit 144609046a6fade9d17e6a74e4cfd06935573288.

---
+++ nav2_bringup/launch/rviz_launch.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.events import Shutdown
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import LaunchConfigAsBool
+
+
+def generate_launch_description() -> LaunchDescription:
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('nav2_bringup')
+
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration('namespace')
+    rviz_config_file = LaunchConfiguration('rviz_config')
+    use_sim_time = LaunchConfigAsBool('use_sim_time')
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='navigation',
+        description=(
+            'Top-level namespace. The value will be used to replace the '
+            '<robot_namespace> keyword on the rviz config file.'
+        ),
+    )
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config',
+        default_value=os.path.join(bringup_dir, 'rviz', 'nav2_default_view.rviz'),
+        description='Full path to the RVIZ config file to use',
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+
+    # Launch rviz
+    start_rviz_cmd = Node(
+        package='rviz2',
+        executable='rviz2',
+        namespace=namespace,
+        arguments=['-d', rviz_config_file],
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        remappings=[
+            ('/tf', 'tf'),
+            ('/tf_static', 'tf_static'),
+        ],
+    )
+
+    exit_event_handler = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=start_rviz_cmd,
+            on_exit=EmitEvent(event=Shutdown(reason='rviz exited')),
+        ),
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # Add any conditioned actions
+    ld.add_action(start_rviz_cmd)
+
+    # Add other nodes and processes we need
+    ld.add_action(exit_event_handler)
+
+    return ld
---
+++ nav2_controller/include/nav2_controller/plugins/simple_progress_checker.hpp
@@ -0,0 +1,129 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
+#define NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
+
+#include <string>
+#include <vector>
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_ros_common/lifecycle_node.hpp"
+#include "nav2_core/progress_checker.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+
+namespace nav2_controller
+{
+/**
+* @class SimpleProgressChecker
+* @brief This plugin is used to check the position of the robot to make sure
+* that it is actually progressing towards a goal.
+*/
+
+class SimpleProgressChecker : public nav2_core::ProgressChecker
+{
+public:
+  /**
+   * @brief Construct a new Simple Progress Checker object
+   */
+  SimpleProgressChecker() = default;
+
+  /**
+   * @brief Destroy the Simple Progress Checker object
+   */
+  ~SimpleProgressChecker();
+
+  /**
+   * @brief Initialize the goal checker
+   * @param parent Weak pointer to the lifecycle node
+   * @param plugin_name Name of the plugin
+   */
+  void initialize(
+    const nav2::LifecycleNode::WeakPtr & parent,
+    const std::string & plugin_name) override;
+
+  /**
+   * @brief Checks if the robot has moved compare to previous
+   * @param current_pose Current pose of the robot
+   * @return true, if the robot has moved enough, false otherwise
+   */
+  bool check(geometry_msgs::msg::PoseStamped & current_pose) override;
+
+  /**
+   * @brief Reset the progress checker state
+   */
+  void reset() override;
+
+protected:
+  /**
+   * @brief Calculates robots movement from baseline pose
+   * @param pose Current pose of the robot
+   * @return true, if movement is greater than radius_, or false
+   */
+  bool isRobotMovedEnough(const geometry_msgs::msg::Pose & pose);
+  /**
+   * @brief Resets baseline pose with the current pose of the robot
+   * @param pose Current pose of the robot
+   */
+  void resetBaselinePose(const geometry_msgs::msg::Pose & pose);
+
+  /**
+   * @brief Calculates distance between two poses
+   * @param pose1 First pose
+   * @param pose2 Second pose
+   * @return Distance between the two poses
+   */
+  static double pose_distance(
+    const geometry_msgs::msg::Pose &,
+    const geometry_msgs::msg::Pose &);
+
+  nav2::LifecycleNode::WeakPtr node_;
+  rclcpp::Clock::SharedPtr clock_;
+  rclcpp::Logger logger_{rclcpp::get_logger("simple_progress_checker")};
+
+  double radius_;
+  rclcpp::Duration time_allowance_{0, 0};
+
+  geometry_msgs::msg::Pose baseline_pose_;
+  rclcpp::Time baseline_time_;
+
+  bool baseline_pose_set_{false};
+  // Dynamic parameters handler
+  std::mutex mutex_;
+  rclcpp::node_interfaces::PostSetParametersCallbackHandle::SharedPtr post_set_params_handler_;
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr on_set_params_handler_;
+  std::string plugin_name_;
+
+  /**
+   * @brief Validate incoming parameter updates before applying them.
+   * This callback is triggered when one or more parameters are about to be updated.
+   * It checks the validity of parameter values and rejects updates that would lead
+   * to invalid or inconsistent configurations
+   * @param parameters List of parameters that are being updated.
+   * @return rcl_interfaces::msg::SetParametersResult Result indicating whether the update is accepted.
+   */
+  rcl_interfaces::msg::SetParametersResult validateParameterUpdatesCallback(
+    const std::vector<rclcpp::Parameter> & parameters);
+
+  /**
+   * @brief Apply parameter updates after validation
+   * This callback is executed when parameters have been successfully updated.
+   * It updates the internal configuration of the node with the new parameter values.
+   * @param parameters List of parameters that have been updated.
+   */
+  void updateParametersCallback(const std::vector<rclcpp::Parameter> & parameters);
+};
+}  // namespace nav2_controller
+
+#endif  // NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
