6b37768e81ece4d555ce803d40dee2e9fc3c9ca6
SteveMacenski <stevenmacenski@gmail.com>
2026-08-12 00:48:58 +0000

    Revert "Trimmed the branch to only the nav2_loopback_sim sub directory"
    This reverts commit bbb73c85bbed79dfcae7c748170994ea4955e07a.

---
+++ nav2_controller/plugins/pose_progress_checker.cpp
@@ -0,0 +1,144 @@
+// Copyright (c) 2023 Dexory
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nav2_controller/plugins/pose_progress_checker.hpp"
+#include <cmath>
+#include <string>
+#include <memory>
+#include <vector>
+#include "angles/angles.h"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "nav2_ros_common/node_utils.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "tf2/utils.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+using rcl_interfaces::msg::ParameterType;
+using std::placeholders::_1;
+
+namespace nav2_controller
+{
+
+PoseProgressChecker::~PoseProgressChecker()
+{
+  auto node = node_.lock();
+  if (post_set_params_handler_ && node) {
+    node->remove_post_set_parameters_callback(post_set_params_handler_.get());
+  }
+  post_set_params_handler_.reset();
+  if (on_set_params_handler_ && node) {
+    node->remove_on_set_parameters_callback(on_set_params_handler_.get());
+  }
+  on_set_params_handler_.reset();
+}
+
+void PoseProgressChecker::initialize(
+  const nav2::LifecycleNode::WeakPtr & parent,
+  const std::string & plugin_name)
+{
+  plugin_name_ = plugin_name;
+  SimpleProgressChecker::initialize(parent, plugin_name);
+  node_ = parent;
+  auto node = node_.lock();
+  logger_ = node->get_logger();
+
+  required_movement_angle_ = node->declare_or_get_parameter(
+    plugin_name + ".required_movement_angle", 0.5);
+
+  // Add callback for dynamic parameters
+  post_set_params_handler_ = node->add_post_set_parameters_callback(
+    std::bind(
+      &PoseProgressChecker::updateParametersCallback,
+      this, std::placeholders::_1));
+  on_set_params_handler_ = node->add_on_set_parameters_callback(
+    std::bind(
+      &PoseProgressChecker::validateParameterUpdatesCallback,
+      this, std::placeholders::_1));
+}
+
+bool PoseProgressChecker::check(geometry_msgs::msg::PoseStamped & current_pose)
+{
+  std::lock_guard<std::mutex> lock_reinit(mutex_);
+  // relies on short circuit evaluation to not call is_robot_moved_enough if
+  // baseline_pose is not set.
+  if (!baseline_pose_set_ || PoseProgressChecker::isRobotMovedEnough(current_pose.pose)) {
+    resetBaselinePose(current_pose.pose);
+    return true;
+  }
+  return clock_->now() - baseline_time_ <= time_allowance_;
+}
+
+bool PoseProgressChecker::isRobotMovedEnough(const geometry_msgs::msg::Pose & pose)
+{
+  return pose_distance(pose, baseline_pose_) > radius_ ||
+         poseAngleDistance(pose, baseline_pose_) > required_movement_angle_;
+}
+
+double PoseProgressChecker::poseAngleDistance(
+  const geometry_msgs::msg::Pose & pose1,
+  const geometry_msgs::msg::Pose & pose2)
+{
+  double theta1 = tf2::getYaw(pose1.orientation);
+  double theta2 = tf2::getYaw(pose2.orientation);
+  return std::abs(angles::shortest_angular_distance(theta1, theta2));
+}
+
+rcl_interfaces::msg::SetParametersResult
+PoseProgressChecker::validateParameterUpdatesCallback(
+  const std::vector<rclcpp::Parameter> & parameters)
+{
+  rcl_interfaces::msg::SetParametersResult result;
+  result.successful = true;
+  for (auto parameter : parameters) {
+    const auto & param_type = parameter.get_type();
+    const auto & param_name = parameter.get_name();
+    if (param_name.find(plugin_name_ + ".") != 0) {
+      continue;
+    }
+    if (param_type == ParameterType::PARAMETER_DOUBLE) {
+      if (parameter.as_double() < 0.0) {
+        RCLCPP_WARN(
+        logger_, "The value of parameter '%s' is incorrectly set to %f, "
+        "it should be >=0. Ignoring parameter update.",
+        param_name.c_str(), parameter.as_double());
+        result.successful = false;
+      }
+    }
+  }
+  return result;
+}
+
+void
+PoseProgressChecker::updateParametersCallback(
+  const std::vector<rclcpp::Parameter> & parameters)
+{
+  std::lock_guard<std::mutex> lock_reinit(mutex_);
+  for (const auto & parameter : parameters) {
+    const auto & param_type = parameter.get_type();
+    const auto & param_name = parameter.get_name();
+    if (param_name.find(plugin_name_ + ".") != 0) {
+      continue;
+    }
+    if (param_type == ParameterType::PARAMETER_DOUBLE) {
+      if (param_name == plugin_name_ + ".required_movement_angle") {
+        required_movement_angle_ = parameter.as_double();
+      }
+    }
+  }
+}
+
+}  // namespace nav2_controller
+
+PLUGINLIB_EXPORT_CLASS(nav2_controller::PoseProgressChecker, nav2_core::ProgressChecker)
