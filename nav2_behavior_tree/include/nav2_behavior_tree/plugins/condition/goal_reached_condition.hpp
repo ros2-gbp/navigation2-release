99c23c6bf3a05dd0a911384d5357ce4c94e36503
SteveMacenski <stevenmacenski@gmail.com>
2026-08-12 00:49:02 +0000

    Revert "Trimmed the branch to only the nav2_docking/opennav_docking_bt sub directory"
    This reverts commit 540e2a94e012071d3d300325f9274347436a186f.

---
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/goal_reached_condition.hpp
@@ -0,0 +1,111 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__GOAL_REACHED_CONDITION_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__GOAL_REACHED_CONDITION_HPP_
+
+#include <string>
+#include <memory>
+
+#include "nav2_ros_common/lifecycle_node.hpp"
+#include "behaviortree_cpp/condition_node.h"
+#include "behaviortree_cpp/json_export.h"
+#include "nav2_behavior_tree/bt_utils.hpp"
+#include "nav2_behavior_tree/json_utils.hpp"
+#include "nav2_ros_common/tf2_factories.hpp"
+
+
+namespace nav2_behavior_tree
+{
+
+/**
+ * @brief A BT::ConditionNode that returns SUCCESS when a specified goal
+ * is reached and FAILURE otherwise
+ * @note  It will re-initialize when halted.
+ *
+ * Usage in XML:
+ * @code
+ * <GoalReached goal="{goal}" robot_base_frame="base_link"/>
+ * @endcode
+ */
+class GoalReachedCondition : public BT::ConditionNode
+{
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::GoalReachedCondition
+   * @param condition_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  GoalReachedCondition(
+    const std::string & condition_name,
+    const BT::NodeConfiguration & conf);
+
+  GoalReachedCondition() = delete;
+
+  /**
+   * @brief A destructor for nav2_behavior_tree::GoalReachedCondition
+   */
+  ~GoalReachedCondition() override;
+
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  /**
+   * @brief Function to read parameters and initialize class variables
+   */
+  void initialize();
+
+  /**
+   * @brief Checks if the current robot pose lies within a given distance from the goal
+   * @return bool true when goal is reached, false otherwise
+   */
+  bool isGoalReached();
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    // Register JSON definitions for the types used in the ports
+    BT::RegisterJsonDefinition<geometry_msgs::msg::PoseStamped>();
+
+    return {
+      BT::InputPort<geometry_msgs::msg::PoseStamped>("goal", "Destination"),
+      BT::InputPort<std::string>("robot_base_frame", "Robot base frame")
+    };
+  }
+
+protected:
+  /**
+   * @brief Cleanup function
+   */
+  void cleanup()
+  {}
+
+private:
+  nav2::LifecycleNode::SharedPtr node_;
+  nav2::TransformBuffer::SharedPtr tf_;
+
+  double goal_reached_tol_;
+  double transform_tolerance_;
+  std::string robot_base_frame_;
+};
+
+}  // namespace nav2_behavior_tree
+
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__GOAL_REACHED_CONDITION_HPP_
