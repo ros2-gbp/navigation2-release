b45920f7bb34d853d9e907452af906589cd98617
SteveMacenski <stevenmacenski@gmail.com>
2026-08-21 22:24:27 +0000

    Rebase from 'upstream'

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/is_battery_charging_condition.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/is_battery_charging_condition.hpp
@@ -29,6 +29,11 @@ namespace nav2_behavior_tree
 /**
  * @brief A BT::ConditionNode that listens to a battery topic and
  * returns SUCCESS when battery is charging and FAILURE otherwise
+ *
+ * Usage in XML:
+ * @code
+ * <IsBatteryCharging battery_topic="/battery_status"/>
+ * @endcode
  */
 class IsBatteryChargingCondition : public BT::ConditionNode
 {
@@ -58,7 +63,7 @@ public:
   {
     return {
       BT::InputPort<std::string>(
-        "battery_topic", std::string("/battery_status"), "Battery topic")
+        "battery_topic", "/battery_status", "Battery topic")
     };
   }
 
