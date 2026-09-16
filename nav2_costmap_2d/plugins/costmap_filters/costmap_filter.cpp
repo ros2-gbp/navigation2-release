a7ffd225704438ba350318b51576a557ced8b785
SteveMacenski <stevenmacenski@gmail.com>
2026-09-16 18:42:15 +0000

    Revert "Trimmed the branch to only the nav2_behavior_tree sub directory"
    This reverts commit 2669d5c6ccbd92508d81bd264baf1ad8f6b153c2.

---
+++ nav2_costmap_2d/plugins/costmap_filters/costmap_filter.cpp
@@ -0,0 +1,211 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, 2013, Willow Garage, Inc.
+ *  Copyright (c) 2020 Samsung Research Russia
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the <ORGANIZATION> nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Eitan Marder-Eppstein
+ *         David V. Lu!!
+ *         Alexey Merzlyakov
+ *********************************************************************/
+
+#include "nav2_costmap_2d/costmap_filters/costmap_filter.hpp"
+
+#include <exception>
+
+#include "nav2_util/geometry_utils.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+
+#include "nav2_costmap_2d/cost_values.hpp"
+#include "nav2_util/occ_grid_values.hpp"
+
+namespace nav2_costmap_2d
+{
+
+CostmapFilter::CostmapFilter()
+: filter_info_topic_(""), mask_topic_("")
+{
+  access_ = new mutex_t();
+}
+
+CostmapFilter::~CostmapFilter()
+{
+  delete access_;
+}
+
+void CostmapFilter::onInitialize()
+{
+  nav2::LifecycleNode::SharedPtr node = node_.lock();
+  if (!node) {
+    throw std::runtime_error{"Failed to lock node"};
+  }
+
+  try {
+    // Get parameters
+    enabled_ = node->declare_or_get_parameter(name_ + "." + "enabled", true);
+    filter_info_topic_ = node->declare_or_get_parameter<std::string>(name_ + "." +
+      "filter_info_topic");
+    double transform_tolerance = node->declare_or_get_parameter(name_ + "." + "transform_tolerance",
+      0.1);
+    transform_tolerance_ = tf2::durationFromSec(transform_tolerance);
+
+    // Costmap Filter enabling service
+    enable_service_ = node->create_service<std_srvs::srv::SetBool>(
+      name_ + "/toggle_filter",
+      std::bind(
+        &CostmapFilter::enableCallback, this, std::placeholders::_1,
+        std::placeholders::_2, std::placeholders::_3));
+  } catch (const std::exception & ex) {
+    RCLCPP_ERROR(logger_, "Parameter problem: %s", ex.what());
+    throw ex;
+  }
+}
+
+void CostmapFilter::activate()
+{
+  initializeFilter(filter_info_topic_);
+}
+
+void CostmapFilter::deactivate()
+{
+  resetFilter();
+}
+
+void CostmapFilter::reset()
+{
+  resetFilter();
+  initializeFilter(filter_info_topic_);
+  setCurrent(false);
+}
+
+void CostmapFilter::updateBounds(
+  double robot_x, double robot_y, double robot_yaw,
+  double * /*min_x*/, double * /*min_y*/, double * /*max_x*/, double * /*max_y*/)
+{
+  if (!enabled_) {
+    return;
+  }
+
+  latest_pose_.position.x = robot_x;
+  latest_pose_.position.y = robot_y;
+  latest_pose_.position.z = 0.0;
+  latest_pose_.orientation = nav2_util::geometry_utils::orientationAroundZAxis(robot_yaw);
+}
+
+void CostmapFilter::updateCosts(
+  nav2_costmap_2d::Costmap2D & master_grid,
+  int min_i, int min_j, int max_i, int max_j)
+{
+  if (!enabled_) {
+    return;
+  }
+
+  process(master_grid, min_i, min_j, max_i, max_j, latest_pose_);
+  setCurrent(true);
+}
+
+void CostmapFilter::enableCallback(
+  const std::shared_ptr<rmw_request_id_t>/*request_header*/,
+  const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+  std::shared_ptr<std_srvs::srv::SetBool::Response> response)
+{
+  enabled_ = request->data;
+  response->success = true;
+  if (enabled_) {
+    response->message = "Enabled";
+  } else {
+    response->message = "Disabled";
+  }
+}
+
+bool CostmapFilter::transformPose(
+  const std::string global_frame,
+  const geometry_msgs::msg::Pose & global_pose,
+  const std::string mask_frame,
+  geometry_msgs::msg::Pose & mask_pose) const
+{
+  if (mask_frame != global_frame) {
+    // Filter mask and current layer are in different frames:
+    // Transform (global_pose.position.x, global_pose.position.y) point from current layer frame
+    // to mask_pose in mask_frame
+    geometry_msgs::msg::TransformStamped transform;
+    geometry_msgs::msg::PointStamped in, out;
+    in.header.stamp = clock_->now();
+    in.header.frame_id = global_frame;
+    in.point.x = global_pose.position.x;
+    in.point.y = global_pose.position.y;
+    in.point.z = 0.0;
+
+    try {
+      tf_->transform(in, out, mask_frame, transform_tolerance_);
+    } catch (tf2::TransformException & ex) {
+      RCLCPP_ERROR_THROTTLE(
+        logger_, *(clock_), 2000,
+        "CostmapFilter: failed to get costmap frame (%s) "
+        "transformation to mask frame (%s) with error: %s",
+        global_frame.c_str(), mask_frame.c_str(), ex.what());
+      return false;
+    }
+
+    mask_pose = global_pose;
+    mask_pose.position.x = out.point.x;
+    mask_pose.position.y = out.point.y;
+    // mask_pose.position.z is kept as-is (0 or original)
+    // orientation is preserved
+  } else {
+    // Filter mask and current layer are in the same frame:
+    // Just use global_pose
+    mask_pose = global_pose;
+  }
+
+  return true;
+}
+
+unsigned char CostmapFilter::getMaskCost(
+  nav_msgs::msg::OccupancyGrid::ConstSharedPtr filter_mask,
+  const unsigned int mx, const unsigned int & my) const
+{
+  const unsigned int index = my * filter_mask->info.width + mx;
+
+  const signed char data = filter_mask->data[index];
+  if (data == nav2_util::OCC_GRID_UNKNOWN) {
+    return NO_INFORMATION;
+  } else {
+    // Linear conversion from OccupancyGrid data range [OCC_GRID_FREE..OCC_GRID_OCCUPIED]
+    // to costmap data range [FREE_SPACE..LETHAL_OBSTACLE]
+    return std::round(
+      static_cast<double>(data) * (LETHAL_OBSTACLE - FREE_SPACE) /
+      (nav2_util::OCC_GRID_OCCUPIED - nav2_util::OCC_GRID_FREE));
+  }
+}
+
+}  // namespace nav2_costmap_2d
