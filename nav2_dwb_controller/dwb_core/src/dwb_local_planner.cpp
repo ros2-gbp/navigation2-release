ab8026d52dbff9d98b0a5f2e9984312665620283
SteveMacenski <stevenmacenski@gmail.com>
2026-09-16 00:38:07 +0000

    Revert "Trimmed the branch to only the nav2_dwb_controller/nav_2d_msgs sub directory"
    This reverts commit 1739c1426cf095f0e554bc40f191b34aa72ae72d.

---
+++ nav2_dwb_controller/dwb_core/src/dwb_local_planner.cpp
@@ -0,0 +1,402 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "dwb_core/dwb_local_planner.hpp"
+#include "dwb_core/exceptions.hpp"
+#include "dwb_core/illegal_trajectory_tracker.hpp"
+#include "dwb_msgs/msg/critic_score.hpp"
+#include "nav_2d_msgs/msg/twist2_d.hpp"
+#include "nav_2d_utils/conversions.hpp"
+#include "nav2_util/geometry_utils.hpp"
+#include "nav2_ros_common/lifecycle_node.hpp"
+#include "nav2_core/controller_exceptions.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "nav2_ros_common/tf2_factories.hpp"
+
+using nav2_util::geometry_utils::euclidean_distance;
+
+namespace dwb_core
+{
+
+DWBLocalPlanner::DWBLocalPlanner()
+: traj_gen_loader_("dwb_core", "dwb_core::TrajectoryGenerator"),
+  critic_loader_("dwb_core", "dwb_core::TrajectoryCritic")
+{
+}
+
+void DWBLocalPlanner::configure(
+  const nav2::LifecycleNode::WeakPtr & parent,
+  std::string name, nav2::TransformBuffer::SharedPtr tf,
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
+{
+  node_ = parent;
+  auto node = node_.lock();
+
+  logger_ = node->get_logger();
+  clock_ = node->get_clock();
+  costmap_ros_ = costmap_ros;
+  tf_ = tf;
+  dwb_plugin_name_ = name;
+
+  debug_trajectory_details_ = node->declare_or_get_parameter(
+    dwb_plugin_name_ + ".debug_trajectory_details", false);
+  std::string traj_generator_name = node->declare_or_get_parameter(
+    dwb_plugin_name_ + ".trajectory_generator_name",
+    std::string("dwb_plugins::StandardTrajectoryGenerator"));
+  short_circuit_trajectory_evaluation_ = node->declare_or_get_parameter(
+    dwb_plugin_name_ + ".short_circuit_trajectory_evaluation", true);
+
+  pub_ = std::make_unique<DWBPublisher>(node, dwb_plugin_name_);
+  pub_->on_configure();
+
+  traj_generator_ = traj_gen_loader_.createUniqueInstance(traj_generator_name);
+
+  traj_generator_->initialize(node, dwb_plugin_name_);
+
+  try {
+    loadCritics();
+  } catch (const std::exception & e) {
+    RCLCPP_ERROR(logger_, "Couldn't load critics! Caught exception: %s", e.what());
+    throw nav2_core::ControllerException(
+            "Couldn't load critics! Caught exception: " +
+            std::string(e.what()));
+  }
+}
+
+void
+DWBLocalPlanner::activate()
+{
+  pub_->on_activate();
+  traj_generator_->activate();
+}
+
+void
+DWBLocalPlanner::deactivate()
+{
+  pub_->on_deactivate();
+  traj_generator_->deactivate();
+}
+
+void
+DWBLocalPlanner::cleanup()
+{
+  pub_->on_cleanup();
+
+  traj_generator_.reset();
+}
+
+std::string
+DWBLocalPlanner::resolveCriticClassName(std::string base_name)
+{
+  if (base_name.find("Critic") == std::string::npos) {
+    base_name = base_name + "Critic";
+  }
+
+  if (base_name.find("::") == std::string::npos) {
+    for (unsigned int j = 0; j < default_critic_namespaces_.size(); j++) {
+      std::string full_name = default_critic_namespaces_[j] + "::" + base_name;
+      if (critic_loader_.isClassAvailable(full_name)) {
+        return full_name;
+      }
+    }
+  }
+  return base_name;
+}
+
+void
+DWBLocalPlanner::loadCritics()
+{
+  auto node = node_.lock();
+  if (!node) {
+    throw std::runtime_error{"Failed to lock node"};
+  }
+
+  default_critic_namespaces_ = node->declare_or_get_parameter(
+    dwb_plugin_name_ + ".default_critic_namespaces",
+    std::vector<std::string>());
+  if (default_critic_namespaces_.empty()) {
+    default_critic_namespaces_.emplace_back("dwb_critics");
+  }
+
+  std::vector<std::string> critic_names =
+    node->declare_or_get_parameter<std::vector<std::string>>(
+      dwb_plugin_name_ + ".critics");
+  if (critic_names.empty()) {
+    throw std::runtime_error("No critics defined for " + dwb_plugin_name_);
+  }
+
+  for (unsigned int i = 0; i < critic_names.size(); i++) {
+    std::string critic_plugin_name = critic_names[i];
+
+    std::string plugin_class = node->declare_or_get_parameter(
+      dwb_plugin_name_ + "." + critic_plugin_name + ".class",
+      critic_plugin_name);
+
+    plugin_class = resolveCriticClassName(plugin_class);
+
+    TrajectoryCritic::Ptr plugin = critic_loader_.createUniqueInstance(plugin_class);
+    RCLCPP_INFO(
+      logger_,
+      "Using critic \"%s\" (%s)", critic_plugin_name.c_str(), plugin_class.c_str());
+    critics_.push_back(plugin);
+    try {
+      plugin->initialize(node, critic_plugin_name, dwb_plugin_name_, costmap_ros_);
+    } catch (const std::exception & e) {
+      RCLCPP_ERROR(logger_, "Couldn't initialize critic plugin!");
+      throw nav2_core::ControllerException(
+              "Couldn't initialize critic plugin: " +
+              std::string(e.what()));
+    }
+    RCLCPP_INFO(logger_, "Critic plugin initialized");
+  }
+}
+
+void
+DWBLocalPlanner::newPathReceived(const nav_msgs::msg::Path & /*raw_global_path*/)
+{
+  for (TrajectoryCritic::Ptr & critic : critics_) {
+    critic->reset();
+  }
+  traj_generator_->reset();
+}
+
+geometry_msgs::msg::TwistStamped
+DWBLocalPlanner::computeVelocityCommands(
+  const geometry_msgs::msg::PoseStamped & pose,
+  const geometry_msgs::msg::Twist & velocity,
+  nav2_core::GoalChecker * /*goal_checker*/,
+  const nav_msgs::msg::Path & transformed_global_plan,
+  const geometry_msgs::msg::PoseStamped & global_goal)
+{
+  std::shared_ptr<dwb_msgs::msg::LocalPlanEvaluation> results = nullptr;
+  if (pub_->shouldRecordEvaluation()) {
+    results = std::make_shared<dwb_msgs::msg::LocalPlanEvaluation>();
+  }
+
+  try {
+    nav_2d_msgs::msg::Twist2DStamped cmd_vel2d = computeVelocityCommands(
+      pose,
+      nav_2d_utils::twist3Dto2D(velocity), results, transformed_global_plan, global_goal);
+    pub_->publishEvaluation(results);
+    geometry_msgs::msg::TwistStamped cmd_vel;
+    cmd_vel.twist = nav_2d_utils::twist2Dto3D(cmd_vel2d.velocity);
+    return cmd_vel;
+  } catch (const nav2_core::ControllerTFError & e) {
+    pub_->publishEvaluation(results);
+    throw e;
+  } catch (const nav2_core::InvalidPath & e) {
+    pub_->publishEvaluation(results);
+    throw e;
+  } catch (const nav2_core::NoValidControl & e) {
+    pub_->publishEvaluation(results);
+    throw e;
+  } catch (const nav2_core::ControllerException & e) {
+    pub_->publishEvaluation(results);
+    throw e;
+  }
+}
+
+nav_2d_msgs::msg::Twist2DStamped
+DWBLocalPlanner::computeVelocityCommands(
+  const geometry_msgs::msg::PoseStamped & pose,
+  const nav_2d_msgs::msg::Twist2D & velocity,
+  std::shared_ptr<dwb_msgs::msg::LocalPlanEvaluation> & results,
+  const nav_msgs::msg::Path & transformed_global_plan,
+  const geometry_msgs::msg::PoseStamped & global_goal)
+{
+  if (results) {
+    results->header.frame_id = pose.header.frame_id;
+    results->header.stamp = clock_->now();
+  }
+
+  nav2_costmap_2d::Costmap2D * costmap = costmap_ros_->getCostmap();
+  std::unique_lock<nav2_costmap_2d::Costmap2D::mutex_t> lock(*(costmap->getMutex()));
+
+  for (TrajectoryCritic::Ptr & critic : critics_) {
+    if (!critic->prepare(pose.pose, velocity, global_goal.pose, transformed_global_plan)) {
+      RCLCPP_WARN(rclcpp::get_logger("DWBLocalPlanner"), "A scoring function failed to prepare");
+    }
+  }
+
+  try {
+    dwb_msgs::msg::TrajectoryScore best = coreScoringAlgorithm(pose.pose, velocity, results);
+
+    // Return Value
+    nav_2d_msgs::msg::Twist2DStamped cmd_vel;
+    cmd_vel.header.stamp = clock_->now();
+    cmd_vel.velocity = best.traj.velocity;
+
+    // debrief stateful scoring functions
+    for (TrajectoryCritic::Ptr & critic : critics_) {
+      critic->debrief(cmd_vel.velocity);
+    }
+
+    lock.unlock();
+
+    pub_->publishLocalPlan(pose.header, best.traj);
+    pub_->publishCostGrid(costmap_ros_, critics_);
+
+    return cmd_vel;
+  } catch (const dwb_core::NoLegalTrajectoriesException & e) {
+    nav_2d_msgs::msg::Twist2D empty_cmd;
+    dwb_msgs::msg::Trajectory2D empty_traj;
+    // debrief stateful scoring functions
+    for (TrajectoryCritic::Ptr & critic : critics_) {
+      critic->debrief(empty_cmd);
+    }
+
+    lock.unlock();
+
+    pub_->publishLocalPlan(pose.header, empty_traj);
+    pub_->publishCostGrid(costmap_ros_, critics_);
+
+    throw nav2_core::NoValidControl(
+            "Could not find a legal trajectory: " +
+            std::string(e.what()));
+  }
+}
+
+dwb_msgs::msg::TrajectoryScore
+DWBLocalPlanner::coreScoringAlgorithm(
+  const geometry_msgs::msg::Pose & pose,
+  const nav_2d_msgs::msg::Twist2D velocity,
+  std::shared_ptr<dwb_msgs::msg::LocalPlanEvaluation> & results)
+{
+  nav_2d_msgs::msg::Twist2D twist;
+  dwb_msgs::msg::Trajectory2D traj;
+  dwb_msgs::msg::TrajectoryScore best, worst;
+  best.total = -1;
+  worst.total = -1;
+  IllegalTrajectoryTracker tracker;
+
+  traj_generator_->startNewIteration(velocity);
+  while (traj_generator_->hasMoreTwists()) {
+    twist = traj_generator_->nextTwist();
+    traj = traj_generator_->generateTrajectory(pose, velocity, twist);
+
+    try {
+      dwb_msgs::msg::TrajectoryScore score = scoreTrajectory(traj, best.total);
+      tracker.addLegalTrajectory();
+      if (results) {
+        results->twists.push_back(score);
+      }
+      if (best.total < 0 || score.total < best.total) {
+        best = score;
+        if (results) {
+          results->best_index = results->twists.size() - 1;
+        }
+      }
+      if (worst.total < 0 || score.total > worst.total) {
+        worst = score;
+        if (results) {
+          results->worst_index = results->twists.size() - 1;
+        }
+      }
+    } catch (const dwb_core::IllegalTrajectoryException & e) {
+      if (results) {
+        dwb_msgs::msg::TrajectoryScore failed_score;
+        failed_score.traj = traj;
+
+        dwb_msgs::msg::CriticScore cs;
+        cs.name = e.getCriticName();
+        cs.raw_score = -1.0;
+        failed_score.scores.push_back(cs);
+        failed_score.total = -1.0;
+        results->twists.push_back(failed_score);
+      }
+      tracker.addIllegalTrajectory(e);
+    }
+  }
+
+  if (best.total < 0) {
+    if (debug_trajectory_details_) {
+      RCLCPP_ERROR(rclcpp::get_logger("DWBLocalPlanner"), "%s", tracker.getMessage().c_str());
+      for (auto const & x : tracker.getPercentages()) {
+        RCLCPP_ERROR(
+          rclcpp::get_logger(
+            "DWBLocalPlanner"), "%.2f: %10s/%s", x.second,
+          x.first.first.c_str(), x.first.second.c_str());
+      }
+    }
+    throw NoLegalTrajectoriesException(tracker);
+  }
+
+  return best;
+}
+
+dwb_msgs::msg::TrajectoryScore
+DWBLocalPlanner::scoreTrajectory(
+  const dwb_msgs::msg::Trajectory2D & traj,
+  double best_score)
+{
+  dwb_msgs::msg::TrajectoryScore score;
+  score.traj = traj;
+
+  for (TrajectoryCritic::Ptr & critic : critics_) {
+    dwb_msgs::msg::CriticScore cs;
+    cs.name = critic->getName();
+    cs.scale = critic->getScale();
+
+    if (cs.scale == 0.0) {
+      score.scores.push_back(cs);
+      continue;
+    }
+
+    double critic_score = critic->scoreTrajectory(traj);
+    cs.raw_score = critic_score;
+    score.scores.push_back(cs);
+    score.total += critic_score * cs.scale;
+    if (short_circuit_trajectory_evaluation_ && best_score > 0 && score.total > best_score) {
+      // since we keep adding positives, once we are worse than the best, we will stay worse
+      break;
+    }
+  }
+
+  return score;
+}
+
+}  // namespace dwb_core
+
+// Register this controller as a nav2_core plugin
+PLUGINLIB_EXPORT_CLASS(
+  dwb_core::DWBLocalPlanner,
+  nav2_core::Controller)
