1310d1df5616b9a0d60356f02cc23890fa701de7
SteveMacenski <stevenmacenski@gmail.com>
2026-09-16 18:43:36 +0000

    Revert "Trimmed the branch to only the nav2_behaviors sub directory"
    This reverts commit 033ee78d55f5ad45a3fa10024c78b7ca8340d799.

---
+++ nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.hpp
@@ -0,0 +1,210 @@
+// Copyright (c) 2020 Shrijit Singh
+// Copyright (c) 2020 Samsung Research America
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_REGULATED_PURE_PURSUIT_CONTROLLER__REGULATED_PURE_PURSUIT_CONTROLLER_HPP_
+#define NAV2_REGULATED_PURE_PURSUIT_CONTROLLER__REGULATED_PURE_PURSUIT_CONTROLLER_HPP_
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <mutex>
+
+#include "nav2_core/controller.hpp"
+#include "nav2_ros_common/lifecycle_node.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "nav2_regulated_pure_pursuit_controller/collision_checker.hpp"
+#include "nav2_regulated_pure_pursuit_controller/parameter_handler.hpp"
+#include "nav2_regulated_pure_pursuit_controller/regulation_functions.hpp"
+#include "nav2_regulated_pure_pursuit_controller/dynamic_window_pure_pursuit_functions.hpp"
+#include "nav2_ros_common/tf2_factories.hpp"
+
+namespace nav2_regulated_pure_pursuit_controller
+{
+
+/**
+ * @class nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
+ * @brief Regulated pure pursuit controller plugin
+ */
+class RegulatedPurePursuitController : public nav2_core::Controller
+{
+public:
+  /**
+   * @brief Constructor for nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
+   */
+  RegulatedPurePursuitController() = default;
+
+  /**
+   * @brief Destrructor for nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
+   */
+  ~RegulatedPurePursuitController() override = default;
+
+  /**
+   * @brief Configure controller state machine
+   * @param parent WeakPtr to node
+   * @param name Name of plugin
+   * @param tf TF buffer
+   * @param costmap_ros Costmap2DROS object of environment
+   */
+  void configure(
+    const nav2::LifecycleNode::WeakPtr & parent,
+    std::string name, nav2::TransformBuffer::SharedPtr tf,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+
+  /**
+   * @brief Cleanup controller state machine
+   */
+  void cleanup() override;
+
+  /**
+   * @brief Activate controller state machine
+   */
+  void activate() override;
+
+  /**
+   * @brief Deactivate controller state machine
+   */
+  void deactivate() override;
+
+  /**
+   * @brief Compute the best command given the current pose and velocity, with possible debug information
+   * @param pose      Current robot pose
+   * @param velocity  Current robot velocity
+   * @param goal_checker   Ptr to the goal checker for this task in case useful in computing commands
+   * @param transformed_global_plan The global plan after being processed by the path handler
+   * @param global_goal The last pose of the global plan
+   * @return          Best command
+   */
+  geometry_msgs::msg::TwistStamped computeVelocityCommands(
+    const geometry_msgs::msg::PoseStamped & pose,
+    const geometry_msgs::msg::Twist & velocity,
+    nav2_core::GoalChecker * /*goal_checker*/,
+    const nav_msgs::msg::Path & transformed_global_plan,
+    const geometry_msgs::msg::PoseStamped & global_goal) override;
+
+  bool cancel() override;
+
+  /**
+   * @brief nav2_core newPathReceived - Receives a new plan from the Planner Server
+   * @param raw_global_path The global plan from the Planner Server
+   */
+  void newPathReceived(const nav_msgs::msg::Path & raw_global_path) override;
+
+  /**
+   * @brief Limits the maximum linear speed of the robot.
+   * @param speed_limit expressed in absolute value (in m/s)
+   * or in percentage from maximum robot speed.
+   * @param percentage Setting speed limit in percentage if true
+   * or in absolute values in false case.
+   */
+  void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
+
+  void reset() override;
+
+protected:
+  /**
+   * @brief Get lookahead distance
+   * @param cmd the current speed to use to compute lookahead point
+   * @return lookahead distance
+   */
+  double getLookAheadDistance(const geometry_msgs::msg::Twist &);
+
+  /**
+   * @brief Creates a PointStamped message for visualization
+   * @param carrot_pose Input carrot point as a PoseStamped
+   * @return CarrotMsg a carrot point marker, PointStamped
+   */
+  std::unique_ptr<geometry_msgs::msg::PointStamped> createCarrotMsg(
+    const geometry_msgs::msg::PoseStamped & carrot_pose);
+
+  /**
+   * @brief Whether robot should rotate to rough path heading
+   * @param carrot_pose current lookahead point
+   * @param angle_to_path Angle of robot output relative to carrot marker
+   * @param x_vel_sign Velocoty sign (forward or backward)
+   * @return Whether should rotate to path heading
+   */
+  bool shouldRotateToPath(
+    const geometry_msgs::msg::PoseStamped & carrot_pose, double & angle_to_path,
+    double & x_vel_sign);
+
+  /**
+   * @brief Whether robot should rotate to final goal orientation
+   * @param goal_checker Goal checker instance for tolerances / state
+   * @param robot_pose Current robot pose in costmap's global frame
+   * @param goal_pose Goal pose in costmap's global frame
+   * @param speed Current robot speed
+   * @param transformed_plan The plan in the robot base frame
+   * @return Whether should rotate to goal heading
+   */
+  bool shouldRotateToGoalHeading(
+    nav2_core::GoalChecker * goal_checker,
+    const geometry_msgs::msg::PoseStamped & robot_pose,
+    const geometry_msgs::msg::PoseStamped & goal_pose,
+    const geometry_msgs::msg::Twist & speed,
+    const nav_msgs::msg::Path & transformed_plan);
+
+  /**
+   * @brief Create a smooth and kinematically smoothed rotation command
+   * @param linear_vel linear velocity
+   * @param angular_vel angular velocity
+   * @param angle_to_path Angle of robot output relative to carrot marker
+   * @param curr_speed the current robot speed
+   */
+  void rotateToHeading(
+    double & linear_vel, double & angular_vel,
+    const double & angle_to_path, const geometry_msgs::msg::Twist & curr_speed);
+
+  /**
+   * @brief apply regulation constraints to the system
+   * @param linear_vel robot command linear velocity input
+   * @param lookahead_dist optimal lookahead distance
+   * @param curvature curvature of path
+   * @param speed Speed of robot
+   * @param pose_cost cost at this pose
+   */
+  void applyConstraints(
+    const double & curvature, const geometry_msgs::msg::Twist & speed,
+    const double & pose_cost, const nav_msgs::msg::Path & path,
+    double & linear_vel, double & sign);
+
+  nav2::LifecycleNode::WeakPtr node_;
+  nav2::TransformBuffer::SharedPtr tf_;
+  std::string plugin_name_;
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+  nav2_costmap_2d::Costmap2D * costmap_;
+  rclcpp::Logger logger_ {rclcpp::get_logger("RegulatedPurePursuitController")};
+
+  Parameters * params_;
+  double control_duration_;
+  bool cancelling_ = false;
+  bool finished_cancelling_ = false;
+  bool is_rotating_to_heading_ = false;
+  geometry_msgs::msg::Twist last_command_velocity_;
+
+  nav2::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr carrot_pub_;
+  nav2::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr curvature_carrot_pub_;
+  nav2::Publisher<std_msgs::msg::Bool>::SharedPtr is_rotating_to_heading_pub_;
+  nav2::Publisher<nav_msgs::msg::Path>::SharedPtr carrot_arc_pub_;
+  std::unique_ptr<nav2_regulated_pure_pursuit_controller::ParameterHandler> param_handler_;
+  std::unique_ptr<nav2_regulated_pure_pursuit_controller::CollisionChecker> collision_checker_;
+};
+
+}  // namespace nav2_regulated_pure_pursuit_controller
+
+#endif  // NAV2_REGULATED_PURE_PURSUIT_CONTROLLER__REGULATED_PURE_PURSUIT_CONTROLLER_HPP_
