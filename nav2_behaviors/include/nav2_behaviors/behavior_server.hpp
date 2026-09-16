bed17f3d02067e48dbee7b8980214b5011ef5bef
SteveMacenski <stevenmacenski@gmail.com>
2026-09-16 18:44:11 +0000

    Revert "Trimmed the branch to only the nav2_voxel_grid sub directory"
    This reverts commit 137e166ed807c75cd5ee31b382fb7fc03765a7e6.

---
+++ nav2_behaviors/include/nav2_behaviors/behavior_server.hpp
@@ -0,0 +1,111 @@
+// Copyright (c) 2018 Samsung Research America
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. Reserved.
+
+
+#include <chrono>
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "nav2_ros_common/lifecycle_node.hpp"
+#include "nav2_ros_common/tf2_factories.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "nav2_core/behavior.hpp"
+
+#ifndef NAV2_BEHAVIORS__BEHAVIOR_SERVER_HPP_
+#define NAV2_BEHAVIORS__BEHAVIOR_SERVER_HPP_
+
+namespace behavior_server
+{
+
+/**
+ * @class behavior_server::BehaviorServer
+ * @brief An server hosting a map of behavior plugins
+ */
+class BehaviorServer : public nav2::LifecycleNode
+{
+public:
+  /**
+   * @brief A constructor for behavior_server::BehaviorServer
+   * @param options Additional options to control creation of the node.
+   */
+  explicit BehaviorServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+  ~BehaviorServer();
+
+protected:
+  /**
+   * @brief Loads behavior plugins from parameter file
+   * @return bool if successfully loaded the plugins
+   */
+  bool loadBehaviorPlugins();
+
+  /**
+   * @brief configures behavior plugins
+   */
+  void configureBehaviorPlugins();
+
+  /**
+   * @brief configures behavior plugins
+   */
+  void setupResourcesForBehaviorPlugins();
+  /**
+   * @brief Configure lifecycle server
+   */
+  nav2::CallbackReturn on_configure(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Activate lifecycle server
+   */
+  nav2::CallbackReturn on_activate(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Deactivate lifecycle server
+   */
+  nav2::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Cleanup lifecycle server
+   */
+  nav2::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Shutdown lifecycle server
+   */
+  nav2::CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state) override;
+
+  nav2::TransformBuffer::SharedPtr tf_;
+  nav2::TransformListener::SharedPtr transform_listener_;
+
+  // Plugins
+  pluginlib::ClassLoader<nav2_core::Behavior> plugin_loader_;
+  std::vector<pluginlib::UniquePtr<nav2_core::Behavior>> behaviors_;
+  std::vector<std::string> default_ids_;
+  std::vector<std::string> default_types_;
+  std::vector<std::string> behavior_ids_;
+  std::vector<std::string> behavior_types_;
+
+  // Utilities
+  std::unique_ptr<nav2_costmap_2d::CostmapSubscriber> local_costmap_sub_;
+  std::unique_ptr<nav2_costmap_2d::FootprintSubscriber> local_footprint_sub_;
+  std::shared_ptr<nav2_costmap_2d::CostmapTopicCollisionChecker> local_collision_checker_;
+
+  std::unique_ptr<nav2_costmap_2d::CostmapSubscriber> global_costmap_sub_;
+  std::unique_ptr<nav2_costmap_2d::FootprintSubscriber> global_footprint_sub_;
+  std::shared_ptr<nav2_costmap_2d::CostmapTopicCollisionChecker> global_collision_checker_;
+};
+
+}  // namespace behavior_server
+
+#endif  // NAV2_BEHAVIORS__BEHAVIOR_SERVER_HPP_
